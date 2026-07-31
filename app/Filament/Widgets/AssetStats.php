5898c53f298244e4fd033766872cf9a29d87196a
AZetZet <jamesalejandrox07@gmail.com>
2026-07-31 14:19:55 +0700

    Penambahan detail

--- app/Filament/Widgets/AssetStats.php
+++ app/Filament/Widgets/AssetStats.php
@@ -4,57 +4,57 @@ namespace App\Filament\Widgets;
 
 use App\Models\MstAsset;
 use App\Models\TrxServiceAsset;
-use App\Models\TrxRetireAsset;
-
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-
 class AssetStats extends StatsOverviewWidget
 {
+    /**
+     * Tampilkan 5 widget dalam 1 baris.
+     */
+    protected function getColumns(): int
+    {
+        return 5;
+    }
+
     protected function getStats(): array
     {
         return [
 
             Stat::make(
-                'Total Asset',
+                'Total',
                 MstAsset::count()
             )
-            ->description('Total asset terdaftar')
-            ->color('primary'),
-
-
-            Stat::make(
-                'Asset Not Used',
-                MstAsset::where(
-                    'StatusAsset',
-                    'Not Used'
-                )->count()
-            )
-            ->description('Asset tidak digunakan')
-            ->color('success'),
-
+                ->description('Semua Asset')
+                ->color('primary'),
 
             Stat::make(
-                'Asset Service',
-                TrxServiceAsset::where(
-                    'StatusService',
-                    'Proses'
-                )->count()
+                'Available',
+                MstAsset::where('StatusAsset', 'Available')->count()
             )
-            ->description('Asset sedang diperbaiki')
-            ->color('warning'),
-
+                ->description('Tersedia')
+                ->color('info'),
 
             Stat::make(
-                'Asset Retired',
-                MstAsset::where(
-                    'StatusAsset',
-                    'Retired'
-                )->count()
+                'Not Used',
+                MstAsset::where('StatusAsset', 'Not Used')->count()
             )
-            ->description('Asset Retired')
-            ->color('danger'),
+                ->description('Tidak Dipakai')
+                ->color('success'),
+
+            Stat::make(
+                'Service',
+                TrxServiceAsset::where('StatusService', 'Proses')->count()
+            )
+                ->description('Dalam Service')
+                ->color('warning'),
+
+            Stat::make(
+                'Retired',
+                MstAsset::where('StatusAsset', 'Retired')->count()
+            )
+                ->description('Pensiun')
+                ->color('danger'),
 
         ];
     }
